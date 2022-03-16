46c7f0da53c5ccc1d819fc38de0c25427f49e861
Alexsu12 <asanurb1205@g.educaand.es>
2022-03-16 14:34:58 +0100

    small fixes

--- src/main/kotlin/objetos/Coche.kt
+++ src/main/kotlin/objetos/Coche.kt
@@ -6,6 +6,16 @@ open class Coche (marca: String, puertas: Int){
     fun arrancarCoche(){
         println("Vroom!")
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Coche) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return javaClass.hashCode()
+    }
 }
 
 /*
@@ -13,6 +23,7 @@ Esta es una clase hija. Se usa ":" junto a la superclase.
 Se puede declarar una clase solo con su constructor
 */
 class Turismo(marca: String, puertas: Int, color: String) : Coche(marca, puertas)
+class Furgoneta(viajeNaranjas: Int, viajeChatarra: Int, marca: String, puertas: Int) : Coche(marca, puertas)
 
 fun main() {
     val coche1 = Turismo("Ford", 5, "rojo")

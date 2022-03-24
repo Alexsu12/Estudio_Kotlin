ba715402c621d067268be62e5c873b37f9e7c732
Alexsu12 <asanurb1205@g.educaand.es>
2022-03-24 09:50:19 +0100

    Arregla POO

--- src/main/kotlin/objetos/Coche.kt
+++ src/main/kotlin/objetos/Coche.kt
@@ -1,31 +1,45 @@
 package objetos
 
 // Esto es un constructor de una superclase. Se usa open para las superclases
-open class Coche (marca: String, puertas: Int){
+open class Coche (private var marca: String, private var puertas: Int){
     // Aquí van las funciones de clase
     fun arrancarCoche(){
         println("Vroom!")
     }
 
+    override fun toString(): String {
+        return "Coche(marca='$marca', puertas=$puertas)"
+    }
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is Coche) return false
+
+        if (marca != other.marca) return false
+        if (puertas != other.puertas) return false
+
         return true
     }
 
     override fun hashCode(): Int {
-        return javaClass.hashCode()
+        var result = marca.hashCode()
+        result = 31 * result + puertas
+        return result
     }
 }
 
 /*
 Esta es una clase hija. Se usa ":" junto a la superclase.
-Se puede declarar una clase solo con su constructor
 */
-class Turismo(marca: String, puertas: Int, color: String) : Coche(marca, puertas)
-class Furgoneta(viajeNaranjas: Int, viajeChatarra: Int, marca: String, puertas: Int) : Coche(marca, puertas)
+class Turismo(private var marca: String = "Ford", private var puertas: Int = 5, private var color: String = "Rojo"):
+    Coche(marca, puertas) {
+    override fun toString(): String {
+        return "Turismo(marca='$marca', puertas=$puertas, color='$color')"
+    }
+}
 
 fun main() {
-    val coche1 = Turismo("Ford", 5, "rojo")
+    val coche1 = Turismo()
+    println(coche1)
     println(coche1.arrancarCoche())
 }

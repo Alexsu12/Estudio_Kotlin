a8ebe233f431b3898c262e7838fb4160d14ccaac
Alexsu12 <asanurb1205@g.educaand.es>
2022-03-29 11:54:49 +0200

    Añade POO avanzado

---
+++ src/main/kotlin/objetos_avanzados/ClaseDatos.kt
@@ -0,0 +1,4 @@
+package objetos_avanzados
+
+// Las clases de datos sirven para almacenar datos y traen toString y equals incorporadas
+data class ClaseDatos(val nombre: String="John", val apellidos: String= "Doe")
---
+++ src/main/kotlin/objetos_avanzados/Main.kt
@@ -0,0 +1,7 @@
+package objetos_avanzados
+
+fun main(){
+    val prueba1 = ClaseDatos()
+
+    println(prueba1)
+}
